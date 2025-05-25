a17da1ba369ed8edce013ab3e4bda9ccbd409c23
Alexander Petrov <alpet1@gmail.com>
2025-05-25 09:30:53 +0300

    Fixes for deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 echo "Please configure DB hosts and credentials carefully..."
 sleep 3
-nano lib/db-config.php
+cp lib/db_config.sample.php /lib/db_config.php
+nano lib/db_config.php
 
 curl https://jpgraph.net/download/download.php?p=57 --output jpgraph-4.4.2.tar.gz
 tar -xvf jpgraph-4.4.2.tar.gz
